e9ba0036c1d4bffcb52faded422755950a107b2e
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2024-07-18 20:00:25 -0600

    Seleccionar TODOS
    1

--- graphql-nest/01-foundation/src/todo/todo.controller.ts
+++ graphql-nest/01-foundation/src/todo/todo.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Controller('todo')
 export class TodoController {
@@ -15,7 +16,7 @@ export class TodoController {
   }
 
   @Get()
-  findAll() {
+  findAll(): Todo[] {
     return this.todoService.findAll();
   }
 
